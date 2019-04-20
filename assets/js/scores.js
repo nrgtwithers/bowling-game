4d5b012f2ad4512295c8085e0cee46d04cef33a0
Nina Withers <ninarosewithers@gmail.com>
2019-04-20 16:09:01 -0400

    just making finishing touches to the UI and UX

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,24 @@
 // GAME
 var frames = 10;
 var rolls = 2;
+var scores;
+
+var gutter = 0;
+var strike = 10 + 10 + 10;
+var spare;
+
+// function to add scores
+
+// function gameScores(){
+    
+// }
+
+// gameScores();
+
+
+// Reset
+$("#reset").click(function () {
+    // $(this).hide()
+    console.log('clicked')
+    location.reload();
+})
