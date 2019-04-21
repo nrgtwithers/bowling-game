24066c50b37014e0cf9ea5a609bb2a481c1fe13d
Nina Withers <ninarosewithers@gmail.com>
2019-04-21 18:36:16 -0400

    changes to table

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,58 +5,94 @@ $(function () {
 
 var frames = 10;
 var rolls = 2;
-var scores;
-
-var gutter = 0;
-var strike = 10 + 10 + 10;
-var spare;
-
+var scores= 0;
+var val = [];
 // function to add scores
 
-function gameScores() {
+function gameScores(val) {
     // Button functioning / testing
     $("#pin-0").click(function () {
-        console.log(`Gutterball, bummer!`)
+        console.log(`Gutterball, bummer!`);
+        // add value
+        var val = 0;
+        var zero = $(".pinsHit").text(val);
     })
     $("#pin-1").click(function () {
-        console.log(`1`)
+        console.log(`1`);
+        // add value
+        var val = 1;
+        var one = $(".pinsHit").text(val);
     })
     $("#pin-2").click(function () {
-        console.log(`2`)
+        console.log(`2`);
+        // add value
+        var val = 2;
+        var two = $(".pinsHit").text(val);
     })
     $("#pin-3").click(function () {
-        console.log(`3`)
+        console.log(`3`);
+        // add value
+        var val = 3;
+        var three = $(".pinsHit").text(val);
     })
     $("#pin-4").click(function () {
-        console.log(`4`)
+        console.log(`4`);
+        // add value
+        var val = 4;
+        var four = $(".pinsHit").text(val);
     })
     $("#pin-5").click(function () {
-        console.log(`5`)
+        console.log(`5`);
+        // add value
+        var val = 5;
+        var five = $(".pinsHit").text(val);
     })
     $("#pin-6").click(function () {
-        console.log(`6`)
+        console.log(`6`);
+        // add value
+        var val = 6;
+        var six = $(".pinsHit").text(val);
     })
     $("#pin-7").click(function () {
-        console.log(`7`)
+        console.log(`7`);
+        // add value
+        var val = 7;
+        var seven = $(".pinsHit").text(val);
     })
     $("#pin-8").click(function () {
-        console.log(`8`)
+        console.log(`8`);
+        // add value
+        var val = 8;
+        var eight = $(".pinsHit").text(val);
+    
     })
     $("#pin-9").click(function () {
-        console.log(`9`)
+        console.log(`9`);
+        // add value
+        var val = 9;
+        var nine = $(".pinsHit").text(val);
     })
     $("#pin-10").click(function () {
-        console.log(`STRIKE!`)
+        console.log(`STRIKE!`);
+        // add value
+        var strike = 10 + 10 + 10;
+        var ten = $(".pinsHit").text(strike);
     })
+
+// formula
+
+var scores = val + val
+
 }
 gameScores();
 
 // Totals User Input Frames Scores
-function totalScores() {
+// function totalScores(scores) {
+//     $('#total').empty('');
+//     $('#total').text(scores)
+// }
 
-}
-
-totalScores();
+// totalScores();
 
 
 // Reset
